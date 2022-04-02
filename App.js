9e436262ce7705e7f5ed3f1e1dcdad10e1141d18
JohanTT <thejohan39@gmail.com>
2022-04-02 14:50:11 +0700

    update 2.4

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, TextInput, TouchableOpacity} from 'react-native';
 
-export default function App() {
+import styles from './styles';
+
+const App = () => {
+  const [task, setTask] = useState('');
+  const [listTask, setListTask] = useState([]);
+
+  const renderTask = () => {
+    const data = listTask.filter(task => !task.isDone);
+    if (!data.length) {
+      return <Text>Chua co viec can lam</Text>;
+    }
+
+    return data.map(task => {
+      if (!task.isDone) {
+        return (
+          <TouchableOpacity
+            key={task.name}
+            style={styles.taskContainer}
+            onPress={() => {
+              const name = task.name;
+              const index = listTask.findIndex(item => item.name === name);
+              const newTasks = [...listTask];
+              newTasks[index].isDone = true;
+              setListTask(newTasks);
+            }}>
+            <Text style={styles.taskName}>{task.name}</Text>      
+          </TouchableOpacity>
+        );
+      }
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.title}>TODO APP</Text>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => {
+            setListTask(listTask.concat([{name: task, isDone: false}]));
+            setTask('');
+          }}>
+          <Text style={styles.btnTitle}>+</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TextInput
+            onChangeText={text => setTask(text)}
+            placeholder="Add note"
+            style={styles.input}
+          />
+      </View>
+      <Text style={styles.todoTitle}>To do list</Text>
+      <View style={styles.taskWrapper}>{renderTask()}
+      {listTask.map((task, index) => {
+        if (task.isDone) {
+          return (
+            <TouchableOpacity
+              key={task.name}
+              style={styles.taskContainer}
+              onPress={() => {
+                console.log(index);
+              }}>
+              <Text style={styles.taskDone}>{task.name}</Text>
+            </TouchableOpacity>
+          );
+        }
+      })}
+      </View>
+      
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
